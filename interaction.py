8c12ea237e44c75e0068e58546b9595009865a63
marcoslora <marcoslora95@gmail.com>
2022-07-06 00:05:43 -0400

    Update interaction.py

--- interaction.py
+++ interaction.py
@@ -1,5 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 chrome_driver_path = "/Users/ml/Documents/Development/chromedriver"
-driver = webdriver.Chrome(executable_path=chrome_driver_path)
-driver.get("https://en.wikipedia.org/wiki/Main_Page")
+driver = webdriver.Chrome(chrome_driver_path)
+driver.get("http://secure-retreat-92358.herokuapp.com/")
+# count_wiki = driver.find_element(By.CSS_SELECTOR, "#articlecount a")
+# count_wiki.click()
+# print(count_wiki.text)
+# all_portals = driver.find_element(By.LINK_TEXT, "Contributions")
+# all_portals.click()
+
+f_name = driver.find_element(By.NAME, "fName")
+f_name.send_keys("Marcos")
+l_name = driver.find_element(By.NAME, "lName")
+l_name.send_keys("Lora")
+email = driver.find_element(By.NAME, "email")
+email.send_keys("marcoslsfasf@asdasd.com")
+submit = driver.find_element(By.CSS_SELECTOR, "form button")
+submit.click()
+# email.send_keys(Keys.ENTER)
+
+# driver.quit()
